8fa21475cedd7424fdf207c1f833d9c4b193f8fa
pergunt <ivanroskishny@gmail.com>
2021-03-29 14:09:17 +0300

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,6 +10,8 @@ import "./Home.css";
 
 import { API } from "aws-amplify";
 
+import { Link } from "react-router-dom";
+
 function loadNotes() {
   return API.get("notes", "/notes");
 }
@@ -68,6 +70,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
+        <div className="pt-3">
+          <Link to="/login" className="btn btn-info btn-lg mr-3">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
